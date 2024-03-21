c9a0417390ead41de5dc3812a4754e53daf38a1f
Fernando <lunaf91@gmail.com>
2024-03-20 22:44:54 -0700

    create UserRepository Interface

--- backend/src/main/java/com/smartbudget/repository/UserRepository.java
+++ backend/src/main/java/com/smartbudget/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.smartbudget.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<> {
+import com.smartbudget.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
