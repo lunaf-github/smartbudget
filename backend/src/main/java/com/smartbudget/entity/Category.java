5bd662550b495783f338013a8ed9b8e6b28abd34
Fernando <lunaf91@gmail.com>
2024-03-23 19:25:44 -0700

    Add missing entity annotation

--- backend/src/main/java/com/smartbudget/entity/Category.java
+++ backend/src/main/java/com/smartbudget/entity/Category.java
@@ -2,17 +2,27 @@ package com.smartbudget.entity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
-@Table(name = "Categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
+@Entity
+@Table(name = "Categories")
 public class Category {
 
     @Id
@@ -23,8 +33,8 @@ public class Category {
     @Column(name = "name")
     private String name;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "fk_category_id", referencedColumnName = "category_id")
-    private List<Transaction> transactions;
+    // @OneToMany(cascade = CascadeType.ALL)
+    // @JoinColumn(name = "fk_category_id", referencedColumnName = "category_id")
+    // private List<Transaction> transactions;
 
 }
