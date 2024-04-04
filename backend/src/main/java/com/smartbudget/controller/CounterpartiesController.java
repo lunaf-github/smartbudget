f30c0d12b213151b5fcdb9533f8ebaaf8e6614b0
Fernando <lunaf91@gmail.com>
2024-04-03 21:23:13 -0700

    Add counterparty controller and add entity annotation to entity

---
+++ backend/src/main/java/com/smartbudget/controller/CounterpartiesController.java
@@ -0,0 +1,30 @@
+package com.smartbudget.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smartbudget.dto.CounterpartyDto;
+import com.smartbudget.service.CounterpartyService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/counterparties")
+public class CounterpartiesController {
+
+    private CounterpartyService counterpartyService;
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CounterpartyDto addCounterpart(@RequestBody CounterpartyDto counterpartyDto) {
+        System.out.println("hello");
+        CounterpartyDto savedCounterparty = counterpartyService.createCounterparty(counterpartyDto);
+        return savedCounterparty;
+    }
+
+}
